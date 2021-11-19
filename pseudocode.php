d624c94ce7289ef6e98aca2ccbc258c736ab6e49
Ana Carolina <93962031+anacarolinacdsr@users.noreply.github.com>
2021-11-18 18:34:02 -0800

    Add files via upload

---
+++ pseudocode.php
@@ -0,0 +1,23 @@
+<?php
+echo "Pseudocode"
+?>
+
+<?php
+// Logical Operators
+$x = 10;
+$y = 20;
+
+if ($x ==$y || 1 == 1){
+    echo "True";
+}
+
+?>
+
+<?php
+function remove_duplicates_list($list1) {
+  $nums_unique = array_values(array_unique($list1));
+  return $nums_unique ;
+}
+$nums = array(1,1,2,2,3,4,5,5);
+print_r(remove_duplicates_list($nums));
+?>
